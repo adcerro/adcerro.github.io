6cfb8fd2bb7847e4188893556c32e4618370ce23
adcerro <adcerro@uninorte.edu.co>
2023-07-06 22:58:38 -0500

    abstraction and coloring

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'tools.dart';
 
@@ -11,13 +13,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    const greenColor = Color.fromRGBO(12, 115, 0, 1);
+    const greenColor = Color.fromRGBO(12, 43, 12, 1);
+    const greenColorL = Color.fromRGBO(12, 115, 0, 1);
     return MaterialApp(
       title: 'Spoticharts',
       theme: ThemeData(
-        primaryColor: greenColor,
-        scaffoldBackgroundColor: Colors.black,
-      ),
+          cardColor: greenColor,
+          primaryColor: greenColorL,
+          indicatorColor: Colors.white,
+          scaffoldBackgroundColor: Colors.black,
+          highlightColor: Colors.white,
+          fontFamily: 'SourceSans'),
       home: const MyHomePage(title: 'Spoticharts'),
     );
   }
@@ -41,30 +47,44 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    Tools tool = Tools(Theme.of(context).primaryColor, Colors.black);
+    Tools tool = Tools(Theme.of(context), 'SourceSans');
+    tool.setTBpadding(20);
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Theme.of(context).primaryColor,
-        items: <BottomNavigationBarItem>[
-          tool.bottonButtonBuilder(Icons.home_filled, 'Home'),
-          tool.bottonButtonBuilder(Icons.search, 'Compare'),
-          tool.bottonButtonBuilder(Icons.mail, 'About us')
-        ],
-        selectedItemColor: Colors.white,
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(15),
-        child: Column(children: [
-          RichText(
-            text: const TextSpan(
-              text: 'Hi, this is spoticharts',
-              style: TextStyle(color: Colors.white),
-              children: <TextSpan>[TextSpan(text: '👋')],
-            ),
-          ),
-        ]),
+      bottomNavigationBar: tool.bottomBar(buttons: <BottomNavigationBarItem>[
+        tool.bottonButtonBuilder(Icons.home_filled, 'Home'),
+        tool.bottonButtonBuilder(Icons.search, 'Compare'),
+        tool.bottonButtonBuilder(Icons.mail, 'About us')
+      ], currentIndex: _selectedIndex, function: _onItemTapped),
+      body: Container(
+        padding: const EdgeInsets.all(20),
+        alignment: Alignment.center,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            tool.textWriter(
+                text: 'Hi, this is Spoticharts 👋',
+                size: 25,
+                weight: FontWeight.bold),
+            tool.textWriter(
+                text:
+                    "Let's give a try to our page, we have many options to explore",
+                size: 35,
+                weight: FontWeight.bold),
+            Divider(color: Theme.of(context).primaryColor),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                tool.paragraphWithTitle(
+                    titleSize: 25,
+                    title: 'What Spoticharts does',
+                    textSize: 20,
+                    text:
+                        'Spoticharts is a website that is customized according to your musical tastes, allows you to discover related music and offers you the possibility to compare the popularity of songs and playlists over time, giving you a more complete and enriching music experience.'),
+                const Icon(Icons.piano)
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/tools.dart
+++ lib/tools.dart
@@ -1,14 +1,78 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class Tools {
-  Color mainColor = Colors.blue;
-  Color secondColor = Colors.white;
-  Tools(this.mainColor, this.secondColor);
+  late ThemeData theme;
+  String font = 'SourceSans';
+  double pad = 0;
+  String text = '';
+  Tools(this.theme, this.font);
   IconButton buttonBuilder(IconData icon, action) {
     return IconButton(onPressed: action, icon: Icon(icon));
   }
 
   BottomNavigationBarItem bottonButtonBuilder(IconData icon, String label) {
     return BottomNavigationBarItem(icon: Icon(icon), label: label);
+  }
+
+  void setTBpadding(double num) {
+    pad = num;
+  }
+
+  BottomNavigationBar bottomBar(
+      {required List<BottomNavigationBarItem> buttons,
+      int currentIndex = 0,
+      required Function(int) function}) {
+    return BottomNavigationBar(
+      backgroundColor: theme.cardColor,
+      items: buttons,
+      selectedItemColor: theme.indicatorColor,
+      unselectedItemColor: theme.primaryColor,
+      currentIndex: currentIndex,
+      onTap: function,
+    );
+  }
+
+  Container textWriter(
+      {String text = '', double size = 0, FontWeight? weight}) {
+    TextStyle st = TextStyle(
+        fontSize: size,
+        fontWeight: weight,
+        color: theme.primaryColor,
+        fontFamily: font);
+    Text tx = Text(text, style: st);
+    return Container(
+      padding: EdgeInsets.fromLTRB(0, pad, 0, pad),
+      child: tx,
+    );
+  }
+
+  Container paragraphWithTitle(
+      {String title = '',
+      double titleSize = 0,
+      String text = '',
+      double textSize = 0}) {
+    TextStyle titleStile = TextStyle(
+        fontSize: titleSize,
+        fontWeight: FontWeight.bold,
+        color: theme.primaryColor,
+        fontFamily: font);
+    TextStyle textStile =
+        titleStile.copyWith(fontWeight: FontWeight.normal, fontSize: textSize);
+
+    return Container(
+        child: RichText(
+      overflow: TextOverflow.ellipsis,
+      maxLines: 11,
+      text: TextSpan(
+        text: '$title\n',
+        style: titleStile,
+        children: <TextSpan>[
+          TextSpan(text: text, style: textStile),
+        ],
+      ),
+    ));
+    ;
   }
 }
