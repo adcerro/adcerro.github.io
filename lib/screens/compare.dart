48af1da201ac02c5ba42e5c0d4ab749782e8cb5b
adcerro <adcerro@uninorte.edu.co>
2023-08-03 17:09:56 -0500

    attemps to connect to spotify

--- lib/screens/compare.dart
+++ lib/screens/compare.dart
@@ -1,11 +1,31 @@
+import 'dart:js_interop';
+
 import 'package:flutter/material.dart';
 import 'package:spoticharts/tools.dart';
+import 'dart:async';
+import 'package:spotify/spotify.dart';
 
 class Compare {
   late BuildContext context;
+  String accessToken = '';
   Compare(this.context);
 
+  List<Widget> widgetFiller(Future<Iterable<PlayHistory>> iterable) {
+    List<Widget> list = [];
+    return list;
+  }
+
   Widget horizontalLayout(Tools tool) {
+    final credentials = SpotifyApiCredentials(
+        '55d23f0684a24570bb4e3cb9b59cacbf', '77429b32ade848e19cce3769cc7e10bd');
+    final grant = SpotifyApi.authorizationCodeGrant(credentials);
+    final authUri = grant.getAuthorizationUrl(
+      Uri.parse('https://localhost:8080'),
+      scopes: [], // scopes are optional
+    );
+    final spotify =
+        SpotifyApi.fromAuthCodeGrant(grant, 'https://localhost:8080');
+    List<Widget> list = [];
     return CustomScrollView(
       slivers: [
         SliverAppBar(
@@ -13,30 +33,30 @@ class Compare {
           floating: true,
           title: Row(children: [
             TextButton(
-                onPressed: () => {print('pressed1')},
-                child: Text('Query🧾'),
+                onPressed: () async =>
+                    {list = widgetFiller(spotify.me.recentlyPlayed().all())},
                 style: TextButton.styleFrom(
-                    foregroundColor: Theme.of(context).primaryColor)),
+                    foregroundColor: Theme.of(context).primaryColor),
+                child: Text('Query🧾')),
             TextButton(
-                onPressed: () => {print('pressed2')},
-                child: Text('Current⏲'),
+                onPressed: () => {},
                 style: TextButton.styleFrom(
-                    foregroundColor: Theme.of(context).primaryColor)),
+                    foregroundColor: Theme.of(context).primaryColor),
+                child: Text('Current⏲')),
             TextButton(
-                onPressed: () => {print('pressed3')},
-                child: Text('👩‍🎤Top artists👨‍🎤'),
+                onPressed: () => {},
                 style: TextButton.styleFrom(
-                    foregroundColor: Theme.of(context).primaryColor)),
+                    foregroundColor: Theme.of(context).primaryColor),
+                child: Text('👩‍🎤Top artists👨‍🎤')),
             TextButton(
-                onPressed: () => {print('pressed4')},
-                child: Text('Top songs 🎵🔥'),
+                onPressed: () => {},
                 style: TextButton.styleFrom(
-                    foregroundColor: Theme.of(context).primaryColor))
+                    foregroundColor: Theme.of(context).primaryColor),
+                child: Text('Top songs 🎵🔥'))
           ]),
         ),
         SliverList(
-          delegate: SliverChildListDelegate(
-              [IconButton(onPressed: () => {}, icon: Icon(Icons.abc))]),
+          delegate: SliverChildListDelegate(list),
         )
       ],
     );
