ecce1c27f6ffac312c28e99cbe21122d34e3410f
adcerro <adcerro@uninorte.edu.co>
2023-09-16 08:12:28 -0500

    prisma orm removed

--- lib/screens/compare.dart
+++ lib/screens/compare.dart
@@ -1,35 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:logger/logger.dart';
-import 'package:flutter/foundation.dart';
 
 class Compare extends StatefulWidget {
-  Compare({super.key});
+  const Compare({super.key});
   @override
   State<Compare> createState() => _CompareState();
 }
 
 class _CompareState extends State<Compare> {
-  var db;
-  var logger = Logger();
-  List<Widget> _widgetFiller(iterable) {
-    List<Widget> list = [];
-    try {
-      for (var element in iterable) {
-        list.add(Text(element));
-      }
-    } catch (error) {
-      logger.w('🚨 hey! i did not get something to iterate');
-    }
-    return list;
-  }
-
-  void _connectingDB() async {
-    if (kIsWeb) {
-    } else {}
-  }
-
   Widget _horizontalLayout() {
-    _connectingDB();
     var list;
     return CustomScrollView(
       slivers: [
@@ -38,12 +16,7 @@ class _CompareState extends State<Compare> {
           floating: true,
           title: Row(children: [
             TextButton(
-                onPressed: () => {
-                      setState(() async {
-                        list = _widgetFiller(
-                            db.rawquery('select top 1 from charts'));
-                      })
-                    },
+                onPressed: () => {},
                 style: TextButton.styleFrom(
                     foregroundColor: Theme.of(context).primaryColor),
                 child: const Text('Query🧾')),
